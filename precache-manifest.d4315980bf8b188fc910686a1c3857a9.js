fbe17f4cec8757d7f4380696bd7c10beb0607222
Zakaria EL AISSAOUI <zakaria.elaissaoui.insa@gmail.com>
2020-04-03 19:43:51 +0200

    Updates

---
+++ precache-manifest.d4315980bf8b188fc910686a1c3857a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8b57d52f2d253e1075b81df1dc72e58",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1c865bad4e3037e8c6cc",
+    "url": "/monsters-rolodex/static/css/main.48837f56.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c865bad4e3037e8c6cc",
+    "url": "/monsters-rolodex/static/js/main.afa2db66.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
